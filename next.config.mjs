ee61b87a366c7baef73d18d2a3c78ef583dcc1de
StenLy <kaw(ek+ig;vug)veu%heRc&egeM>
2026-06-21 22:52:54 +0200

    add image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const isDev = process.env.NODE_ENV === "development";
+
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    dangerouslyAllowLocalIP: isDev,
+    qualities: [40, 70, 75],
+    imageSizes: [160, 240, 320, 480],
+    deviceSizes: [640, 750, 1080, 1920],
+    remotePatterns: [
+      { protocol: "https", hostname: "*.strapiapp.com" },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 31536000,
+  },
 };
 
 export default nextConfig;
